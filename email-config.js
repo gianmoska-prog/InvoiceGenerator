82d3432e367560acd34d81dd05107b793e17bb44
gianmoska-prog <gianmoska@gmail.com>
2026-09-09 16:44:24 +0200

    Fix contacts loader script tag

--- email-config.js
+++ email-config.js
@@ -2,4 +2,4 @@
 // Leave empty for Gmail compose + manual attachment. See docs/gmail-setup.md.
 window.MOSCATELLI_EMAIL_CONFIG = Object.freeze({ googleClientId: '866393770239-jcfj24i846ins7l9df8buchcgu5k4ah2.apps.googleusercontent.com' });
 // Load the small contacts/download extension before email.js and app.js initialise.
-document.write('<link rel="stylesheet" href="contacts.css?v=contacts-1"><script src="contacts-bootstrap.js?v=contacts-1"><\/script>');
+document.write('<link rel="stylesheet" href="contacts.css?v=contacts-1"><script src="contacts-bootstrap.js?v=contacts-1"></script>');
